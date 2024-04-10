e41b2d73046989ed6c62bb7a23f6665a7f8a04f0
Lucas Álek <lucas.maistro@gmail.com>
2024-04-10 20:37:33 -0300

    add client requirements 2

--- src/app/academia-emocional/page.js
+++ src/app/academia-emocional/page.js
@@ -1,5 +1,37 @@
+import Image from "next/image";
+import styles from "../about/styles.module.scss";
+import Contact from "../components/ContactSection/Contact";
+import "../globals.scss";
+import "./styles.scss";
+
 function EmotionalAcademy() {
-  return <h1>Academia emocional</h1>;
+  return (
+    <>
+      <section className={styles.section}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/academia-emocional.jpeg"
+            alt="Profile picture from Tatiana Gomes"
+            width={674}
+            height={110}
+            priority
+          />
+        </div>
+        <div className={styles.textContainer}>
+          <h1>Academia emocional</h1>
+          <h2>Agenda fechada no momento!</h2>
+        </div>
+      </section>
+      <Image
+        src="/divider-bottom.png"
+        alt="section divider"
+        width={1440}
+        height={186}
+        className="divider"
+      />
+      <Contact />
+    </>
+  );
 }
 
 export default EmotionalAcademy;
